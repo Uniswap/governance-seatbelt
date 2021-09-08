1b08ac383c0f0f781dc29054dcde2ff414f5a164
Moody Salem <moody.salem@gmail.com>
2021-09-08 14:28:49 -0500

    create an issue in the github action

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { Octokit } from "@octokit/action";
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const octokit = new Octokit();
+
+async function main() {
+  const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    owner,
+    repo,
+    title: "My test issue",
+  });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
