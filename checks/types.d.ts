07427470678f36c1138be9f5838ec97c15bd9ed3
Moody Salem <moody.salem@gmail.com>
2021-09-08 20:53:58 -0500

    active proposals

---
+++ checks/types.d.ts
@@ -0,0 +1,13 @@
+import { BigNumber } from "ethers";
+
+export interface Proposal {
+  id: BigNumber;
+  proposer: string;
+  targets: string[];
+  values: BigNumber[];
+  signatures: string[];
+  calldatas: string[];
+  startBlock: BigNumber;
+  endBlock: BigNumber;
+  description: string;
+}
--- index.ts
+++ index.ts
@@ -1,37 +1,40 @@
 import { GITHUB_REPO_NAME, GITHUB_REPO_OWNER } from "./utils/constants";
 import { github } from "./utils/clients/github";
 import { governorBravo } from "./utils/contracts/governor-bravo";
+import { provider } from "./utils/clients/ethers";
+import { Proposal } from "./checks/types";
 
 async function main() {
-  const proposals = await governorBravo.queryFilter(
+  const currentBlock = await provider.getBlockNumber();
+  const createProposalLogs = await governorBravo.queryFilter(
     governorBravo.filters.ProposalCreated(),
     0,
     "latest"
   );
 
-  const { data } = await github.issues.create({
+  const activeProposals: Proposal[] = createProposalLogs
+    .filter((proposal) => proposal && proposal.args!.endBlock >= currentBlock)
+    .map((proposal) => {
+      return proposal.args as unknown as Proposal;
+    });
+
+  await github.issues.create({
     owner: GITHUB_REPO_OWNER,
     repo: GITHUB_REPO_NAME,
     title: "Report result",
-    body: `# Proposals
-${proposals.reduce((memo, { args }) => {
-  if (!args) return memo;
-  const {
-    id,
-    proposer,
-    targets,
-    values,
-    signatures,
-    calldatas,
-    startBlock,
-    endBlock,
-    description,
-  } = args;
-
-  return `${memo}\n\nproposal id: ${id}; proposer: ${proposer}; targets: ${targets.join(
-    ", "
-  )};`;
-}, "")}
+    body: `# Active proposals
+${activeProposals
+  .map(({ id, proposer, targets, endBlock, startBlock, description }) => {
+    return `
+## Proposal ID: ${id}
+- Proposer: ${proposer}
+- Start Block: ${startBlock}
+- End Block: ${endBlock}
+- Targets: ${targets.join("; ")}
+- Description: ${description}
+`;
+  })
+  .join("\n\n")}
 `,
   });
 }
