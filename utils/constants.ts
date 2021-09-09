6e86ccdd838319faef7a0830bbd9027160e756e3
Moody Salem <moody.salem@gmail.com>
2021-09-08 20:44:36 -0500

    push tsconfig.json

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,6 @@
+export const REPOSITORY: string = process.env.GITHUB_REPOSITORY!;
 export const [GITHUB_REPO_OWNER, GITHUB_REPO_NAME] =
-  process.env.GITHUB_REPOSITORY?.split("/") ?? [];
-export const GOVERNOR_ADDRESS = process.env.GOVERNOR_ADDRESS;
-export const INFURA_API_KEY = process.env.INFURA_API_KEY;
-export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+  typeof REPOSITORY === "string" ? REPOSITORY.split("/") : [];
+export const GOVERNOR_ADDRESS: string = process.env.GOVERNOR_ADDRESS!;
+export const INFURA_API_KEY: string = process.env.INFURA_API_KEY!;
+export const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY!;
