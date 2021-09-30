395be42a1031e0f445037b927cf39d48ce88d445
Matt Solomon <matt@mattsolomon.dev>
2021-09-30 10:16:56 -0700

    feat: save files locally when not in CI

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,3 +6,4 @@ export const RPC_URL: string = process.env.RPC_URL!;
 export const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY!;
 export const DAO_NAME: string = process.env.DAO_NAME!;
 export const REPORTS_BRANCH = "reports";
+export const RUNNING_LOCALLY = Boolean(process.env.RUNNING_LOCALLY) // use 1 or 0 because Boolean('false') = true
