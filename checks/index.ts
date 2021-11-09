b83372f2a05a8ad244afc1b0ce1754c10499ab9b
Matt <matt@mattsolomon.dev>
2021-11-09 13:33:41 -0800

    feat: add proposal check that decodes calldata (#34)
    * feat: add proposal check that decodes calldata
    
    * chore: add comments and remove unnecessary if statement
    
    * fix: calldata decoding now uses the warning field when it cant decode something

--- checks/index.ts
+++ checks/index.ts
@@ -2,10 +2,11 @@ import {
   checkTargetsVerifiedEtherscan,
   checkTouchedContractsVerifiedEtherscan,
 } from './check-targets-verified-etherscan'
+import { checkDecodeCalldata } from './check-decode-calldata'
 import { ProposalCheck } from '../types'
 
 const ALL_CHECKS: {
   [checkId: string]: ProposalCheck
-} = { checkTargetsVerifiedEtherscan, checkTouchedContractsVerifiedEtherscan }
+} = { checkTargetsVerifiedEtherscan, checkTouchedContractsVerifiedEtherscan, checkDecodeCalldata }
 
 export default ALL_CHECKS
