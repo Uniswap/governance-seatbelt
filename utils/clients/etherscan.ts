0eda10b2054d32d9bfce5f3ec6377340f8a19e43
Matt Solomon <matt@mattsolomon.dev>
2021-11-29 12:50:14 -0800

    feat(wip): compile source code from etherscan with storage layout flag

--- utils/clients/etherscan.ts
+++ utils/clients/etherscan.ts
@@ -1,3 +1,5 @@
+import { artifacts, run } from 'hardhat'
+import fs from 'fs'
 import EtherscanClient from '@ethercast/etherscan-client'
 import { fetchJson } from '@ethersproject/web'
 import { ETHERSCAN_API_KEY } from '../constants'
@@ -21,4 +23,46 @@ export async function getCode(contract: string) {
   const response = (await fetchJson(url)) as { status: string; message: string; result: ContractSource[] }
   if (response.status !== '1') throw new Error(`Error fetching ABI for ${contract}: ${response.message}`)
   return response.result[0]
+}
+
+export async function compileCode(contract: string) {
+  // TODO work in progress
+  const code = await getCode(contract)
+
+  //  For some reason the source code object returned from Etherscan starts and ends with two braces
+  code.SourceCode = code.SourceCode.replace('{{', '{').replace('}}', '}')
+
+  // From the SourceCode field, convert the string to JSON and pull out the source code of each file
+  const sourceCodes = Object.values(JSON.parse(code.SourceCode).sources).map(
+    (src) => (src as { content: string; keccak256: string }).content
+  )
+
+  // Combine source code into one string and dump it to a temporary file.
+  const sourceCode = sourceCodes
+    .reverse() // reverse before joining to ensure contract definitions are in the correct order based on inheritance
+    .join('\n')
+    .replace(/import.*/g, '') // remove all import statements since we have everything in one file
+    .replace(/(?<=(pragma experimental ABIEncoderV2;)[\s\S]+)\1/g, '') // only keep the first instance of pragmas: https://newbedev.com/how-to-replace-all-occurrences-of-a-string-except-the-first-one-in-javascript
+    .replace(/(?<=(pragma abicoder v2;)[\s\S]+)\1/g, '')
+
+  const tempContractFile = 'contracts/tmp.sol'
+  fs.writeFileSync(tempContractFile, sourceCode)
+
+  // Compile source code with storageLayout flag
+  // TODO you cannot call the compile task this way and pass in custom settings. Probably need to create a custom
+  // instance of the compiler class and call its `compile` method: https://github.com/nomiclabs/hardhat/tree/master/packages/hardhat-core/src/internal/solidity/compiler
+  await run('compile', {
+    solidity: {
+      settings: {
+        outputSelection: {
+          '*': {
+            '*': ['storageLayout'],
+          },
+        },
+      },
+    },
+  })
+
+  const { abi, deployedBytecode } = artifacts.readArtifactSync('ModifiedTimelock')
+  fs.unlinkSync(tempContractFile)
 }
