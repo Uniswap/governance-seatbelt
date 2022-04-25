f46d6435ab6bd995c2882767cf7327264431db7d
Matt Solomon <matt@mattsolomon.dev>
2022-04-25 06:32:19 -0700

    Solc check (#62)
    * chore: remove unused printer code
    
    * feat: add solc check, clean up slither check a bit

--- checks/index.ts
+++ checks/index.ts
@@ -5,6 +5,7 @@ import {
 import { checkDecodeCalldata } from './check-decode-calldata'
 import { checkLogs } from './check-logs'
 import { checkSlither } from './check-slither'
+import { checkSolc } from './check-solc'
 import { checkStateChanges } from './check-state-changes'
 import { ProposalCheck } from '../types'
 
@@ -16,6 +17,9 @@ const ALL_CHECKS: {
   checkLogs,
   checkTargetsVerifiedEtherscan,
   checkTouchedContractsVerifiedEtherscan,
+  // The solc check must be run before the slither check, because the compilation exports a zip file
+  // which is consumed by slither. This prevents us from having to compile the contracts twice.
+  checkSolc,
   checkSlither,
 }
 
