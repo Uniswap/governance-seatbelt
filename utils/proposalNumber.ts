388b40e97b6a13b229a122a77e8f696b0e7760b6
Lukas <lukasstrassel@googlemail.com>
2022-07-08 11:33:20 +0200

    ci: have more consistent cache keys (#7)

--- utils/proposalNumber.ts
+++ utils/proposalNumber.ts
@@ -5,12 +5,19 @@ import { aaveGovernanceContract } from './contracts/aave-governance-v2'
 async function proposalNumber() {
   const proposalsCount = await aaveGovernanceContract.getProposalsCount()
   const proposals = [...Array(Number(proposalsCount)).keys()]
-  const json = { include: [] as { DAO_NAME: string; proposals: string }[] }
+  const json = { include: [] as { DAO_NAME: string; proposals: string; cacheKey: string }[] }
   const chunkSize = 10
   for (let i = 0; i < proposals.length; i += chunkSize) {
     const chunk = proposals.slice(i, i + chunkSize)
     // we need to use _ instead of, so we can use it as a cache identifier
-    json.include.push({ DAO_NAME: DAO_NAME as string, proposals: chunk.toString().replace(/,/g, '_') })
+    json.include.push({
+      DAO_NAME: DAO_NAME as string,
+      proposals: chunk.toString().replace(/,/g, '_'),
+      cacheKey: [...Array(Number(chunkSize)).keys()]
+        .map((n) => n + i)
+        .toString()
+        .replace(/,/g, '_'),
+    })
   }
   console.log(`::set-output name=matrix::${JSON.stringify(json)}`)
 }
