3d24ddcadbc9355eb503dd7298bc38c3aab55447
Matt Solomon <matt@mattsolomon.dev>
2022-09-23 09:52:05 -0700

    refactor: remove hardcoded bravo slots

--- checks/check-state-changes.ts
+++ checks/check-state-changes.ts
@@ -1,8 +1,6 @@
 import { getAddress } from '@ethersproject/address'
 import { getContractName } from '../utils/clients/tenderly'
 import { bullet } from '../presentation/report'
-import { getProposalId } from '../utils/contracts/governor'
-import { getBravoSlots } from '../utils//contracts/governor-bravo'
 import { ProposalCheck, StateDiff } from '../types'
 
 /**
@@ -30,9 +28,8 @@ export const checkStateChanges: ProposalCheck = {
       // Check if this is a diff that should be filtered out
       const isGovernor = getAddress(addr) == deps.governor.address
       const isTimelock = getAddress(addr) == deps.timelock.address
-      const isGovernorExecutedSlot = diff.raw[0].key === getBravoSlots(getProposalId(proposal)).canceled // canceled and executed are in same slot
       const isQueuedTx = diff.soltype?.name.includes('queuedTransactions')
-      const shouldSkipDiff = (isGovernor && isGovernorExecutedSlot) || (isTimelock && isQueuedTx)
+      const shouldSkipDiff = isGovernor || (isTimelock && isQueuedTx)
       // Skip diffs as required and add the rest to our diffs object
       if (shouldSkipDiff) return diffs
       else if (!diffs[addr]) diffs[addr] = [diff]
