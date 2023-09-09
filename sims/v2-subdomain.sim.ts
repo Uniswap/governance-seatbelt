732af0183df3bf66324971b8e0992a761ff03165
eek <erin.koen@gmail.com>
2023-09-09 08:31:09 -0400

    feat: add multicall

--- sims/v2-subdomain.sim.ts
+++ sims/v2-subdomain.sim.ts
@@ -8,152 +8,146 @@ import { Interface } from '@ethersproject/abi'
 import { labelhash } from '@ensdomains/ensjs/utils/labels'
 import { namehash } from '@ensdomains/ensjs/utils/normalise'
 import ENSPublicResolverABI from '../utils/abis/ENSPublicResolverABI.json' assert { type: 'json' }
-import { Contract } from 'ethers'
-import { JsonRpcProvider } from '@ethersproject/providers'
-
-
 
 const ensRegistryAbi = [
   'function setSubnodeRecord(bytes32 node, bytes32 label, address owner, address resolver, uint64 ttl) external',
 ]
 
-const provider = new JsonRpcProvider(process.env.RPC_URL)
-
+const ensRegistryAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
 const ensRegistryInterface = new Interface(ensRegistryAbi)
-const ensPublicResolverInterface = new Interface(ENSPublicResolverABI)
-const ensRegistry = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
+
 const ensPublicResolverAddress = '0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41'
+const ensPublicResolverInterface = new Interface(ENSPublicResolverABI)
+
 const timelock = '0x1a9C8182C09F50C8318d769245beA52c32BE35BC'
 const nameHash = namehash("uniswap.eth")
 const labelHash = labelhash("v2deployments")
 const subnameHash = namehash("v2deployments.uniswap.eth")
 
-const ensPublicResolver = await new Contract(ensPublicResolverAddress, ENSPublicResolverABI, provider)
 // generate Optimism bytes
-const optimism = await ensPublicResolver.populateTransaction.setText([
+const optimism = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '10',
   // Value.
   '0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1, 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-]).then(transaction => transaction.data)
+])
 
 // generate arbitrum bytes
-const arbitrum = await ensPublicResolver.populateTransaction.setText([
+const arbitrum = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '42161',
   // Value.
   '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f, 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-]).then(transaction => transaction.data)
+])
 
 // generate avalanche bytes
-const avalance = await ensPublicResolver.populateTransaction.setText([
+const avalanche = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '43114',
   // Value.
   '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f, 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-]).then(transaction => transaction.data)
+])
 
 // generate base bytes
-const base = await ensPublicResolver.populateTransaction.setText([
+const base = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '8453',
   // Value.
   '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f, 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-]).then(transaction => transaction.data)
+])
 
 // generate binance bytes
-const binance = await ensPublicResolver.populateTransaction.setText([
+const binance = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '56',
   // Value.
   '0xf5F4496219F31CDCBa6130B5402873624585615a, 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-]).then(transaction => transaction.data)
+])
 
 // generate polygon bytes
-const polygon = await ensPublicResolver.populateTransaction.setText([
+const polygon = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '137',
   // Value.
   '0xfe5e5D361b2ad62c541bAb87C45a0B9B018389a2, 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-]).then(transaction => transaction.data)
+])
 
 // generate gnosis bytes
-const gnosis = await ensPublicResolver.populateTransaction.setText([
+const gnosis = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '100',
   // Value.
   '0xf5F4496219F31CDCBa6130B5402873624585615a, 0x7146c626be7ee5e70747aa75e295439e643fc034',
-]).then(transaction => transaction.data)
-
+])
 // generate Boba bytes
-const boba = await ensPublicResolver.populateTransaction.setText([
+const boba = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '288',
   // Value.
   '0x6D4528d192dB72E282265D6092F4B872f9Dff69e, 0x53163235746ceb81da32293bb0932e1a599256b4',
-]).then(transaction => transaction.data)
+])
 
 // generate linea bytes
-const linea = await ensPublicResolver.populateTransaction.setText([
+const linea = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '59144',
   // Value.
   '0xd19d4B5d358258f05D7B411E21A1460D11B0876F, 0x056588f18869a626b0Ae9e89f077eFE6BA752633',
-]).then(transaction => transaction.data)
+])
 
 // generate moonbeam bytes
-const moonbeam = await ensPublicResolver.populateTransaction.setText([
+const moonbeam = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '288',
   // Value.
   '0xf5F4496219F31CDCBa6130B5402873624585615a, 0x91FbCAe76de0b852519C26D9f8CA865b5027eeFA',
-]).then(transaction => transaction.data)
+])
 
 // generate celo bytes
-const celo = await ensPublicResolver.populateTransaction.setText([
+const celo = ensPublicResolverInterface.encodeFunctionData('setText', [
   // Node.
   subnameHash,
   // Key.
   '288',
   // Value.
   '0xf5F4496219F31CDCBa6130B5402873624585615a, 0x79a530c8e2fA8748B7B40dd3629C0520c2cCf03f',
-]).then(transaction => transaction.data)
+])
 
 // log outputs for tally
 console.log({
   nameHash,
   labelHash,
-  subnameHash, bytes: [polygon, optimism, avalance, base, arbitrum, binance, gnosis, boba, linea, moonbeam, celo]
+  subnameHash, bytes: [optimism, arbitrum, avalanche, polygon, base, binance, celo, gnosis, boba, moonbeam, linea]
 })
 
 // add subname
 const call1 = {
-  target: ensRegistry, // ENS Registry contract.
+  target: ensRegistryAddress, // ENS Registry.
   calldata: ensRegistryInterface.encodeFunctionData('setSubnodeRecord', [
     nameHash, // Node.
     labelHash, // Label.
     timelock, // Owner.
-    ensPublicResolver, // Resolver.
+    ensPublicResolverAddress, // Resolver.
     0, // TTL.
   ]),
   value: 0,
@@ -162,17 +156,13 @@ const call1 = {
 
 // add text records in multicall
 const call2 = {
-  target: ensPublicResolver, // ENS Public Resolver.
-  calldata: ensPublicResolverInterface.encodeFunctionData('multicall', [
-    polygon, optimism, avalance, base, arbitrum, binance, gnosis, boba, linea, moonbeam, celo
-  ]),
+  target: ensPublicResolverAddress, // ENS Public Resolver.
+  calldata: ensPublicResolverInterface.encodeFunctionData('multicall',
+    [[optimism, arbitrum, avalanche, polygon, base, binance, celo, gnosis, boba, moonbeam, linea]]
+  ),
   value: 0,
   signature: '',
 }
-
-
-
-
 
 const calls = [call1, call2]
 
