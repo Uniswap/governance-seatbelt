69cd65dc699833b85943a47cb1bc5aea783b11a3
robin <robinnagpal.tiet@gmail.com>
2024-01-23 16:48:32 -0500

    Add storing of contracts

--- checks/cross-check-decode-calldata.ts
+++ checks/cross-check-decode-calldata.ts
@@ -2,6 +2,9 @@ import { abis } from './../abis/abis'
 import { ProposalCheck, ProposalData } from '@/types'
 import { Interface, AbiCoder } from '@ethersproject/abi'
 import fs from 'fs'
+import mftch, { FETCH_OPT } from 'micro-ftch'
+// @ts-ignore
+const fetchUrl = mftch.default
 
 interface LookupData {
   [address: string]: {
@@ -67,6 +70,7 @@ export const crossCheckDecodeCalldata: ProposalCheck = {
       const fun = iface.getFunction(functionName)
       const parsedData = iface._decodeParams(fun.inputs, callData)
       // const parsedData = iface.decodeFunctionData(functionName, callData)
+      await storeContractNameAndAbi(target)
       console.log('target:', target)
       console.log('function:', functionName)
       console.log(
@@ -102,4 +106,36 @@ export const crossCheckDecodeCalldata: ProposalCheck = {
 
     return { info: [], warnings: [], errors: [] }
   },
+}
+
+async function storeContractNameAndAbi(addr: string) {
+  const address = addr.toLowerCase()
+  const { contractName, abi } = await getContractNameAndAbi(address)
+  const abiFilePath = `./contracts/${address}.json`
+  const abiFileContent = JSON.stringify({ abi, contractName, address }, null, 2)
+  fs.writeFileSync(abiFilePath, abiFileContent, 'utf-8')
+}
+async function getContractNameAndAbi(address: string) {
+  const contractResponse = await fetchUrl(
+    `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${address}&apikey=${process.env.ETHERSCAN_API_KEY}`,
+  )
+
+  const contractResult = contractResponse.result[0]
+
+  if (contractResult.Implementation) {
+    const implResponse = await fetchUrl(
+      `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${contractResult.Implementation}&apikey=${process.env.ETHERSCAN_API_KEY}`,
+    )
+    const implResult = implResponse.result[0]
+
+    return {
+      contractName: implResult.ContractName,
+      abi: implResult.ABI || [],
+    }
+  }
+
+  return {
+    contractName: contractResult.ContractName,
+    abi: contractResult.ABI || [],
+  }
 }
